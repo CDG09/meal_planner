ceb14377b982b887105e5aa1002a43aa8913eb58
CDG09 <codydg188@gmail.com>
2025-12-09 19:02:00 +0000

    Database template added to the app

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
+#Initialize Flask app
 app = Flask(__name__)
+app.config.from_object(Config)
 
+#Initialize SQLAlchemy
+db = SQLAlchemy(app)
 
+#Model Imports
+from models import User
+
+#Create tables (if they don't exist)
+with app.app_context():
+    db.create_all()
+
+#Routes
 @app.route('/')
 def home():
     return render_template('home.html', active_page='home')
---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///meal_planner.db'
+    SQLALCHEMY_TRACK_MODIFICATION = False
